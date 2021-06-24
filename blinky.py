8fd74a9f0497f2db0c7f6bcedebf606da1fa7b9d
Jamila Tomines <60492912+jamilat@users.noreply.github.com>
2021-06-23 21:10:03 -0700

    Updated and moved blinky.py

--- blinky.py
+++ blinky.py
@@ -5,12 +5,11 @@ from pythonMuse.Muse import Muse
 import matplotlib;
 import sys
 import re
-from enum import Enum
 
-class plotWhat(Enum):
-    wave = 1
-    fft = 2
-    wavelet = 3
+# constants used for data view modes
+wave = 1
+fft = 2
+wavelet = 3
     
 matplotlib.use("TkAgg")  # This option may or may not be needed according to your OS and Python setup (Ipython...)
 # See this link for further details: https://stackoverflow.com/questions/7156058/matplotlib-backends-do-i-care
@@ -18,10 +17,46 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 plt.interactive(False)  # This option may or may not be needed according to your OS and Python setup (Ipython...)
-def getModes():
-    if 'museName' not in modeDict:
-        modeDict['museName'] = 'Muse-3BEA'
-    if ''
+def getModes(d):
+    if 'museName' not in d:
+        d['museName'] = 'Muse-C87E'
+    if 'plotWhat' not in d:
+        d['plotWhat'] = 1
+    if 'plotUpdateInterval' not in d:
+        d['plotUpdateInterval'] = 100
+    if "plotLength" not in d:
+        d['plotLength'] = 512  # Length of the plot (in samples). For a 2 second plot, set this variable to be sampleRate*2
+    if 'sampleRate' not in d:
+        d['sampleRate'] = 256
+    return d
+
+def connectMuse(museObj):
+    for i in range(10):
+        print("Attempting to find " + museObj.target_name + ". Attempt " + str(i + 1) + " of 10...")
+        r = museObj.connect()  # Returns none if not found
+        if r is not None:  # If found
+            batt = r.pullBattery()  # Get battery information from the Muse
+            print('Muse Battery is ' + str(batt))  # Print battery level
+            break  # Get out of the for loop
+        else:  # Muse not found
+            print("No MUSE found, trying again...")
+    # TODO: throw exception if muse not found
+
+def plotMuse(d):
+    fig = plt.figure()  # Open an empty figure
+    fig.canvas.mpl_connect('close_event', close_handle)  # Close_handle will be called upon closing the window
+
+    # Adding four subplots (note the indexing, 3,1,2,4)
+    global ax1, ax2, ax3, ax4
+    ax1 = fig.add_subplot(2, 2, 3)
+    ax2 = fig.add_subplot(2, 2, 1)
+    ax3 = fig.add_subplot(2, 2, 2)
+    ax4 = fig.add_subplot(2, 2, 4)
+
+    if d['plotWhat'] == 1:  # If plot wave is selected
+        # FuncAnimation function runs animateEEG every plotUpdateInterval (ms)
+        ani = animation.FuncAnimation(fig, animateEEG, interval=d['plotUpdateInterval'])
+        plt.show()
 
 def animateEEG(i):  # A function to plot EEG, this will be called every "plotUpdateInterval" ms by FuncAnimation
     muse.updateBuffer()  # Pulls a chunk of EEG data from MUSE hardware, filters them (if applicable) and
@@ -58,11 +93,6 @@ if __name__ == "__main__":
             modeDict[m[1]] = m[2]
 
     modeDict = getModes(modeDict)
-    plotWhat = 3  # Choose what to plot, 1: wave, 2: FFT, 3: wavelet
-    plotUpdateInterval = 100  # How often the plots should be updated
-    plotLength = 512  # Length of the plot (in samples). For a 2 second plot, set this variable to be sampleRate*2
-
-    sampleRate = 256  # The sample rate of the MUSE hardware
 
     # Cutoff frequencies for low/high pass and the notch band-stop filters. Set to -1 to skip the filter
     highFreq = 0.1
@@ -72,31 +102,10 @@ if __name__ == "__main__":
     # (see:https://en.wikipedia.org/wiki/Butterworth_filter)
 
     # Here we create the main object from Muse class. This object (muse) will be used for pulling eeg, plotting, ...
-    muse = Muse(target_name=museName, plotLength=plotLength, sampleRate=sampleRate, highPassFreq=highFreq,
+    muse = Muse(target_name=modeDict['museName'], plotLength=modeDict['plotLength'], sampleRate=modeDict['sampleRate'], highPassFreq=highFreq,
                 lowPassFreq=lowFreq, notchFreq=notchFreq, filterOrder=filterOrder)
 
     # Attempting to find and connect to the target MUSE, will retry 10 times.
-    for i in range(10):
-        print("Attempting to find " + museName + ". Attempt " + str(i + 1) + " of 10...")
-        r = muse.connect()  # Returns none if not found
-        if r is not None:  # If found
-            break  # Get out of the for loop
-        else:  # Muse not found
-            print("No MUSE found, trying again...")
-
-    batt = muse.pullBattery()  # Get battery information from the Muse
-    print('Muse Battery is ' + str(batt))  # Print battery level
-
-    fig = plt.figure()  # Open an empty figure
-    fig.canvas.mpl_connect('close_event', close_handle)  # Close_handle will be called upon closing the window
-
-    # Adding four subplots (note the indexing, 3,1,2,4)
-    ax1 = fig.add_subplot(2, 2, 3)
-    ax2 = fig.add_subplot(2, 2, 1)
-    ax3 = fig.add_subplot(2, 2, 2)
-    ax4 = fig.add_subplot(2, 2, 4)
-
-    if plotWhat == 1:  # If plot wave is selected
-        # FuncAnimation function runs animateEEG every plotUpdateInterval (ms)
-        ani = animation.FuncAnimation(fig, animateEEG, interval=plotUpdateInterval)
-        plt.show()
+    connectMuse(muse)
+    plotMuse(modeDict)
+    
